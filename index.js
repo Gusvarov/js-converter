ad3548f2bebb9f70fb9deaa98088fb3cc3197055
Gusvarov <nazikgta24@gmail.com>
2019-10-16 16:44:00 +0000

    My Code

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-let div = document.createElement('div');
+const moneyValue = document.querySelector('#money-value');
+const moneyCoefficient = document.querySelector('#money-coefficient');
+const converter = document.querySelector('#converter');
+const result = document.querySelector('#result');
+
+converter.addEventListener('click', () => {
+    if ( parseFloat(moneyValue.value) &&  parseFloat(moneyCoefficient.value) ) {
+        let coefficient = moneyCoefficient.value * moneyValue.value;
+        result.value = coefficient;
+    }
+})
 
